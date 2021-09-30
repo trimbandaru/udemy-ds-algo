8a3a774ca18b1cd2870730c420b974b21e76fbf2
N D Trimurthulu Bandaru <trimurthulu.bandaru@gmail.com>
2021-09-30 10:48:20 +0530

    return if found in map

--- hash-table/firstRecurringChar.java
+++ hash-table/firstRecurringChar.java
@@ -1,6 +1,5 @@
 import java.util.Map;
 import java.util.HashMap;
-
 class Main {
   public static void main(String[] args) {
      Character res = findFirstRecChar(new Character[]{'a','b','c','b'});
@@ -11,19 +10,20 @@ class Main {
     Map<Character,Integer> map = new HashMap<Character,Integer>();
     for(int i=0; i<arr.length;i++){
       Integer count = map.get(arr[i]);
-      System.out.println(arr[i] +"-"+count);
+      // System.out.println(arr[i] +"-"+count);
       if(count != null){
-        map.put(arr[i],++count);
+        // map.put(arr[i],++count);
+        return arr[i];
       }else{
         map.put(arr[i],1);
       }
     }
 
-    for(int i=0; i<arr.length;i++){
-      if(map.get(arr[i]) > 1){
-        return arr[i];
-      }
-    }
+    // for(int i=0; i<arr.length;i++){
+    //   if(map.get(arr[i]) > 1){
+    //     return arr[i];
+    //   }
+    // }
     return null;
   }
 }
