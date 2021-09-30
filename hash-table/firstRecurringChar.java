cc49bdc0a4b143845fc300ac7d931f344f1322b6
N D Trimurthulu Bandaru <trimurthulu.bandaru@gmail.com>
2021-09-30 10:43:23 +0530

    first rec char with hash map

---
+++ hash-table/firstRecurringChar.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Main {
+  public static void main(String[] args) {
+     Character res = findFirstRecChar(new Character[]{'a','b','c','b'});
+     System.out.println("repeating char: "+res);
+  }
+
+  private static Character findFirstRecChar(Character[] arr){
+    Map<Character,Integer> map = new HashMap<Character,Integer>();
+    for(int i=0; i<arr.length;i++){
+      Integer count = map.get(arr[i]);
+      System.out.println(arr[i] +"-"+count);
+      if(count != null){
+        map.put(arr[i],++count);
+      }else{
+        map.put(arr[i],1);
+      }
+    }
+
+    for(int i=0; i<arr.length;i++){
+      if(map.get(arr[i]) > 1){
+        return arr[i];
+      }
+    }
+    return null;
+  }
+}
