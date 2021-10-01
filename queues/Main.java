6ea49fff1211d351ec043431e2a82a5dda26d597
N D Trimurthulu Bandaru <trimurthulu.bandaru@gmail.com>
2021-10-01 11:03:02 +0530

    Create Main.java

---
+++ queues/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    Queue que = new Queue();
+    que.enqueue(1);
+    que.enqueue(2);
+    que.print();
+    que.dequeue();
+    que.print();
+  }
+}
